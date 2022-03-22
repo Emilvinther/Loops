edaf486a77c8b0f3fb5ea833214881e3d9bf2719
Emil Vinther Lorentzen <emil380e@zbc.dk>
2022-03-22 11:03:27 +0100

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,22 @@
+﻿//for (int i = 0; i < 100; i++)
+//{
+//    if (i < 50)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+//int i = 0;
+
+//while (i < 50)
+//{
+//    i++;
+//    Console.WriteLine(i);
+//}
+
+for (int i = 100; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+Console.ReadKey();
